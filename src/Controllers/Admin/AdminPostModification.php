078596c8d630306828e7496eb3ded6b6387588a2
gitcmartel <martel.c73@gmail.com>
2023-09-07 16:59:48 +0200

    Adding user function to filter the main menu entries

--- src/Controllers/Admin/AdminPostModification.php
+++ src/Controllers/Admin/AdminPostModification.php
@@ -4,6 +4,8 @@ namespace Application\Controllers\Admin;
 
 use Application\Models\Post;
 use Application\Models\PostRepository;
+use Application\Models\UserRepository;
+use Application\Models\User;
 use Application\Lib\Session;
 
 class AdminPostModification
@@ -15,12 +17,21 @@ class AdminPostModification
                 $postRepository = new PostRepository();
                 $post = $postRepository->getPost($_GET['postId']);
 
+                $userFunction = "";
+                //Get the function of the active user
+                if(isset($_SESSION['userId'])){
+                    $userRepository = new UserRepository();
+                    $user = $userRepository->getUser($_SESSION['userId']);
+                    $userFunction = $user->userFunction;
+                }
+
                 $loader = new \Twig\Loader\FilesystemLoader('templates');
                 $twig = new \Twig\Environment($loader, ['cache' => false]);
                 
                 echo $twig->render('post.twig', [ 
                     'post' => $post, 
-                    'activeUser' => Session::getActiveUser()
+                    'activeUser' => Session::getActiveUser(), 
+                    'userFunction' => $userFunction
                 ]);
             }
         }
