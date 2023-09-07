f97eed7226cd0aad4d617c6c82ca2d28721538bf
gitcmartel <martel.c73@gmail.com>
2023-09-07 15:15:03 +0200

    Unit test UserTest creation

---
+++ test/Models/UserTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Application\Models\User;
+
+class UserTest extends TestCase 
+{
+    /**
+     * Test if the function returns true with a valid parameter
+     */
+    public function testIsCreator()
+    {
+        $user = New User();
+        $user->userFunction = "Createur";
+
+        $this->assertTrue($user->isCreator());
+    }
+
+    /**
+     * Test if the function returns true with a valid parameter
+     */
+    public function testIsCreatorAdmin()
+    {
+        $user = New User();
+        $user->userFunction = "Administrateur";
+
+        $this->assertTrue($user->isCreator());
+    }
+
+    public function testIsAdmin()
+    {
+        $user = New User();
+        $user->userFunction = "Administrateur";
+
+        $this->assertTrue($user->isAdmin());
+    }
+}
