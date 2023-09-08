bc3fe31eb764dd07c7d1cbe85a0333915ccf6903
gitcmartel <martel.c73@gmail.com>
2023-09-08 19:30:46 +0200

    Modifying DEFAULT_IMAGE_POST constant

--- src/Lib/Constants.php
+++ src/Lib/Constants.php
@@ -11,7 +11,7 @@ class Constants
 {
     CONST WEBSITE_DOMAIN = 'http://localhost/blog/';
     CONST IMAGE_POST_PATH = 'img\\img-posts\\';
-    CONST DEFAULT_IMAGE_POST = 'img\\blog-post.svg';
+    CONST DEFAULT_IMAGE_POST = 'blog-post.svg';
     CONST IMAGE_EXTENSIONS = array("jpeg", "jpg", "png", "svg");
     CONST IMAGE_MAX_SIZE = 2097152; // 2 Mo
 }
