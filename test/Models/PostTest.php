cf993c299351d53bfdf6b8de276c1b4f1135fc99
gitcmartel <martel.c73@gmail.com>
2023-09-09 15:38:31 +0200

    Finalizing deleteImage function and unit test

--- test/Models/PostTest.php
+++ test/Models/PostTest.php
@@ -8,14 +8,14 @@ use Application\Lib\Path;
 class PostTest extends TestCase
 {
     /**
-     * 
+     * Test a true result 
      */
     public function testDeleteImageTrue()
     {
         //Copy the default image post in the img-post directory
         $rootPath = dirname(__FILE__, 3) . DIRECTORY_SEPARATOR;
         $defaultPostImagePath = Path::fileBuildPath(array('img', Constants::DEFAULT_IMAGE_POST));
-        $testFilePath = Path::fileBuildPath(array('img', 'img-post', Constants::DEFAULT_IMAGE_POST));
+        $testFilePath = Path::fileBuildPath(array('img', 'img-posts', Constants::DEFAULT_IMAGE_POST));
 
         copy($rootPath . $defaultPostImagePath, $rootPath . $testFilePath);
 
@@ -33,5 +33,24 @@ class PostTest extends TestCase
 
         //Executing test
         $this->assertTrue($result);
+    }
+
+    /**
+     * Test a false result with a file that does not exist
+     */
+    public function testDeleteImageFalse()
+    {
+        //Preparing test environnement
+        $rootPath = dirname(__FILE__, 3) . DIRECTORY_SEPARATOR;
+        $fakeImagePath = Path::fileBuildPath(array('img', 'img-posts', 'fake-image.jpg'));
+
+        $post = new Post();
+        $post->imagePath = $rootPath . $fakeImagePath;
+
+        //Getting the function result
+        $result = $post->deleteImage();
+
+        //Executing test
+        $this->assertFalse($result);
     }
 }
