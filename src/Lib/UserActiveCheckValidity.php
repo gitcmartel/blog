487c67b5b4936024ef89e76421e1c51a899a39a8
gitcmartel <martel.c73@gmail.com>
2023-10-15 12:04:32 +0200

    UserActiveCheckValidity correction

--- src/Lib/UserActiveCheckValidity.php
+++ src/Lib/UserActiveCheckValidity.php
@@ -3,13 +3,30 @@
 namespace Application\Lib;
 
 use Application\Lib\DatabaseConnexion;
+use Application\Models\UserRepository;
+use Application\Models\User;
 
 class UserActiveCheckValidity
 {
     #region Functions
     static function check(array $functions) : bool
     {
+        #region Variables
+
         $userRepository = new UserRepository(new DatabaseConnexion);
+        $user = "";
+
+        #endregion
+
+        #region Tests conditions
+        if(! isset($_SESSION['userId'])){
+            return false;
+        }
+
+        #endregion
+
+        #region Function execution
+
         $user = $userRepository->getUser($_SESSION['userId']);
 
         if($user->getId() == null){
@@ -17,6 +34,8 @@ class UserActiveCheckValidity
         } else {
             return $user->checkValidity($functions);
         }
+
+        #endregion
     }
    #endregion
 }
