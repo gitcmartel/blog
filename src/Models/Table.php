0d402bdba3673c7d1addf0a8c2e9bcea3972cd82
gitcmartel <martel.c73@gmail.com>
2023-10-25 19:28:43 +0200

    Creation of function Hydrate in class Table

--- src/Models/Table.php
+++ src/Models/Table.php
@@ -24,5 +24,16 @@ abstract class Table
         $this->id = $id;
     }
 
+
+    function hydrate(array $data) 
+    {
+        foreach ($data as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
     #endregion
 }
