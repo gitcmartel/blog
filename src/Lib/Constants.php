f05666edadfa8ae4c336f6b3f0f2d4c00ad491af
gitcmartel <martel.c73@gmail.com>
2023-10-26 12:28:31 +0200

    Adding DEFAULT_IMAGE_POST_PATH constant in constants class

--- src/Lib/Constants.php
+++ src/Lib/Constants.php
@@ -12,6 +12,7 @@ class Constants
     CONST WEBSITE_DOMAIN = 'http://localhost/blog/';
     CONST IMAGE_POST_PATH = 'img\\img-posts\\';
     CONST DEFAULT_IMAGE_POST = 'blog-post.svg';
+    CONST DEFAULT_IMAGE_POST_PATH = 'img\\blog-post.svg';
     CONST IMAGE_EXTENSIONS = array("jpeg", "jpg", "png", "svg");
     CONST IMAGE_MAX_SIZE = 2097152; // 2 Mo
 }
