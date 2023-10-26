91462636cf14238aa64bb4831d42df0805a26db3
gitcmartel <martel.c73@gmail.com>
2023-10-26 16:59:24 +0200

    Factoring and optimizing class AdminUserSave

--- src/Controllers/Admin/User/AdminUserSave.php
+++ src/Controllers/Admin/User/AdminUserSave.php
@@ -10,6 +10,7 @@ use Application\Lib\Session;
 use Application\Lib\Password;
 use Application\Lib\TwigLoader;
 use Application\Lib\TwigWarning;
+use Application\Lib\Email;
 
 class AdminUserSave
 {
@@ -18,24 +19,14 @@ class AdminUserSave
     {
         #region Variables
 
-        $warningName = "";
-        $warningSurname = "";
-        $warningPseudo = "";
-        $warningEmail = "";
-        $warningFunction = "";
-        $warningValidity = "";
-        $warningPassword = "";
         $userRepository = new UserRepository(new DatabaseConnexion);
-        $user = new User();
         $twig = TwigLoader::getEnvironment();
 
         #endregion
 
         #region Conditions tests
 
-        if(! UserActiveCheckValidity::check(array('Administrateur'))
-            || ! isset($_POST['userId']) || ! isset($_POST['userName']) || ! isset($_POST['surname']) || ! isset($_POST['pseudo']) || ! isset($_POST['userPwd'])
-            || ! isset($_POST['userPwdConfirmation']) || ! isset($_POST['userFunction']) || ! isset($_POST['userValidity'])){
+        if(! UserActiveCheckValidity::check(array('Administrateur'))){
                 TwigWarning::display(
                     "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site", 
                     "index.php?action=Home\Home", 
@@ -43,87 +34,49 @@ class AdminUserSave
                 return; 
         }
 
-        //Checks if the fields are corrects
-        if(trim($_POST['userName']) === ""){
-            $warningName = "Vous devez renseigner un prénom";
-        } else {
-            $user->setName($_POST['userName']);
+        if (! isset($_POST['userId']) || ! isset($_POST['userName']) || ! isset($_POST['surname']) || ! isset($_POST['pseudo']) || ! isset($_POST['userPwd'])
+        || ! isset($_POST['userPwdConfirmation']) || ! isset($_POST['userFunction']) || ! isset($_POST['userValidity'])){
+            TwigWarning::display(
+                "Une erreur est survenue lors du chargement de la page.", 
+                "index.php?action=Home\Home", 
+                "Retour à la page d'accueil");
+            return;
         }
 
-        if(trim($_POST['surname']) === ""){
-            $warningSurname = "Vous devez renseigner un nom";
-        } else {
-            $user->setSurname($_POST['surname']);
-        }
+        $pageVariables = array(
+            'id' => trim($_POST["userId"]) === '' ? null : intval(trim($_POST["userId"])),
+            'name' => trim($_POST['userName']), 
+            'surname' => trim($_POST['surname']), 
+            'pseudo' => trim($_POST['pseudo']), 
+            'email' => trim($_POST['email']),
+            'function' => trim($_POST['userFunction']), 
+            'isValid' => trim($_POST['userValidity']),
+            'password' => trim($_POST['userPwd']),
+            'passwordConfirmation' => trim($_POST['userPwdConfirmation'])
+        );
 
-        if(trim($_POST['pseudo']) === ""){
-            $warningPseudo = "Vous devez renseigner un pseudo";
-        } else {
-            $user->setPseudo($_POST['pseudo']);
-        }
-
-        if(trim($_POST['userMail']) === ""){
-            $warningEmail = "Vous devez renseigner un email";
-        } else {
-            $emailRegExp = "/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix";
-            if ( ! (preg_match($emailRegExp, trim($_POST['userMail'])) === 1)){
-                $warningEmail = "L'adresse email est incorrecte";
-            }
-            $user->setEmail($_POST['userMail']);
-        }
-
-        if(trim($_POST['userFunction']) === ""){
-            $warningFunction = "Vous devez renseigner une fonction";
-        } else {
-            $user->setUserFunction($_POST['userFunction']);
-        }
-
-        if(trim($_POST['userValidity']) === ""){
-            $warningValidity = "Vous devez selectionner une option";
-        } else {
-            $user->setIsValid($_POST['userValidity']);
-        }
-
-        if((trim($_POST['userPwd']) !== "" || trim($_POST['userPwdConfirmation']) !== "") && 
-            (trim($_POST['userPwd']) !== trim($_POST['userPwdConfirmation']))){
-            $warningPassword = 'Les deux mot de passe ne sont pas identiques';
-        } else {
-            $user->setPassword($_POST['userPwd']);
-        }
-
-        if(! ((trim($_POST['userPwd']) === "") && trim($_POST['userPwdConfirmation']) === "")){
-            if (! Password::checkPassword($_POST['userPwd'])){
-                $warningPassword = "Le mot de passe doit être composé d'au moins 8 caractères, 1 majuscule, 1 minuscule, 1 nombre et 1 caractère spécial";
-            } else {
-                $user->setPassword($_POST['userPwd']);
-            }  
-        }
-
-        //If it is a new user the password fields cannot be empty
-        if(((trim($_POST['userPwd']) === "") && trim($_POST['userPwdConfirmation']) === "") && trim($_POST['userId']) === ""){
-            $warningPassword = "Vous devez renseigner un mot de passe";
-        } else {
-            $user->setPassword($_POST['userPwd']);
-        }
-
-        //Set the user id if there is one. If there is a warning message, then the user id
-        //will get back into the input form field
-        if (trim($_POST['userId']) !== ""){
-            $user->setId($_POST['userId']);
-        }
+        $fieldsWarnings = array(
+            'name' => 'Vous devez renseigner un prénom', 
+            'surname' => 'Vous devez renseigner un nom', 
+            'pseudo' => 'Vous devez renseigner un pseudo', 
+            'email' => Email::checkMailFormat($pageVariables['email']) ? 'Vous devez renseigner un email' : 'L\'adresse email est incorrecte',  
+            'function' => 'Vous devez renseigner une fonction', 
+            'password' => Password::checkPasswordFormFields($pageVariables['password'], $pageVariables['passwordConfirmation'], $pageVariables['id']), 
+            'validity' => 'Vous devez selectionner une option'
+        );
 
         //If there is an incorrect field we display the error message
-        if($warningName !== "" || $warningSurname !== "" || $warningPseudo !== "" || $warningEmail !== "" || 
-         $warningPassword !== "" || $warningFunction !== "" || $warningValidity !== ""){
+        if($pageVariables['name'] !== "" || $pageVariables['surname'] !== "" || $pageVariables['pseudo'] !== "" || $pageVariables['email'] !== "" || 
+        $pageVariables['password'] !== "" || $pageVariables['function'] !== "" || $pageVariables['isValid'] !== ""){
             echo $twig->render('Admin\User\User.html.twig', [ 
-                'warningName' => $warningName, 
-                'warningSurname' => $warningSurname,
-                'warningPseudo' => $warningPseudo,
-                'warningEmail' => $warningEmail, 
-                'warningPassword' => $warningPassword, 
-                'warningFunction' => $warningFunction, 
-                'warningValidity' => $warningValidity, 
-                'user' => $user,
+                'warningName' => $fieldsWarnings['name'], 
+                'warningSurname' => $fieldsWarnings['surname'], 
+                'warningPseudo' => $fieldsWarnings['pseudo'], 
+                'warningEmail' => $fieldsWarnings['email'], 
+                'warningPassword' => $fieldsWarnings['password'], 
+                'warningFunction' => $fieldsWarnings['function'], 
+                'warningValidity' => $fieldsWarnings['validity'],  
+                'user' => $pageVariables['id'],
                 'userFunction' => Session::getActiveUserFunction(),
                 'activeUser' => Session::getActiveUser()
             ]);
@@ -132,10 +85,10 @@ class AdminUserSave
         #endregion
 
         #region Function execution
+        $user = new User($pageVariables);
 
-        if (trim($_POST['userId'] !== "")){
+        if ($pageVariables['id'] !== null){
             //If there is a userId we update
-            $user->setId($_POST['userId']);
             if ($userRepository->updateUser($user)) {
                 //We display the updated user list
                 header("Location:index.php?action=Admin\User\AdminUserList&pageNumber=1");
@@ -147,18 +100,19 @@ class AdminUserSave
                     "Retour à l'accueil");
                 return; 
             }
-        } else { //If there is no userId we create a new user
-            if ($userRepository->createUser($user)){
-                //We display the updated user list
-                header("Location:index.php?action=Admin\User\AdminUserList&pageNumber=1");
-                return;
-            } else {
-                TwigWarning::display(
-                    "Un problème est survenu lors de l'enregistrement de l'utilisateur.", 
-                    "index.php?action=Home\Home", 
-                    "Retour à l'accueil");
-                return; 
-            }
+        }  
+
+        //If there is no userId we create a new user
+        if ($userRepository->createUser($user)){
+            //We display the updated user list
+            header("Location:index.php?action=Admin\User\AdminUserList&pageNumber=1");
+            return;
+        } else {
+            TwigWarning::display(
+                "Un problème est survenu lors de l'enregistrement de l'utilisateur.", 
+                "index.php?action=Home\Home", 
+                "Retour à l'accueil");
+            return; 
         }
 
         #endregion
