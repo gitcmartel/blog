00f5c9a185e3b01e135af0f3fdba654683eef225
gitcmartel <martel.c73@gmail.com>
2023-11-02 11:45:30 +0100

    Creation of class Pseudo

---
+++ src/Lib/Pseudo.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Application\Lib;
+
+use Application\Models\UserRepository;
+
+class Pseudo
+{
+    #region Functions
+
+    static public function checkPseudo(string $pseudo) : string
+    {
+        $userRepository = new UserRepository();
+
+        if($userRepository->exists($pseudo, 'pseudo')){
+            return 'Ce pseudo existe déjà';
+        }
+
+        if(trim($pseudo) === ""){
+            return 'Le champ pseudo doit être complété';
+        }
+
+        return '';
+    }
+
+    #endregion
+}
