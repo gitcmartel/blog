8f6e3c60e169dfed7a1ef63f02a51e712ad7b5b0
gitcmartel <martel.c73@gmail.com>
2023-11-02 09:35:39 +0100

    Creation of function checkIds

--- src/Models/Repository.php
+++ src/Models/Repository.php
@@ -3,6 +3,7 @@
 namespace Application\Models;
 
 use Application\Lib\DatabaseConnexion;
+use PDO;
 
 abstract class Repository
 {
@@ -15,5 +16,27 @@ abstract class Repository
         $this->connexion = new DatabaseConnexion();
     }
 
+    #endregion
+    
+    #region Functions
+
+    /**
+     * Checks if the ids exist in the database
+     * Returns a boolean
+     */
+    public function checkIds(array $ids, string $tableName, string $idFieldName) : bool
+    {
+        $idsString = implode(',', array_map("htmlspecialchars", $ids));
+
+        $statement = $this->connexion->getConnexion()->prepare(
+            "SELECT count(*) as count FROM $tableName WHERE $idFieldName IN ($idsString);"
+        );
+
+        $statement->execute();
+
+        $row = $statement->fetch();
+
+        return ($row['count'] === count($ids));
+    }
     #endregion
 }
