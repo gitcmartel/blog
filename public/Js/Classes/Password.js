dbf6b3fbd8bbab274811ca5bc654443d37196997
gitcmartel <martel.c73@gmail.com>
2023-11-16 10:41:49 +0100

    Modification of class Password.js. Inverting arguments remove and add

--- public/Js/Classes/Password.js
+++ public/Js/Classes/Password.js
@@ -3,12 +3,12 @@ class Password
     static showPassword(passwordElement, iconElement) {
         if (passwordElement.type === "password") {
             passwordElement.type = "text";
-            iconElement.classList.remove('fa-eye');
-            iconElement.classList.add('fa-eye-slash');
+            iconElement.classList.remove('fa-eye-slash');
+            iconElement.classList.add('fa-eye');
           } else {
             passwordElement.type = "password";
-            iconElement.classList.add('fa-eye');
-            iconElement.classList.remove('fa-eye-slash');
+            iconElement.classList.add('fa-eye-slash');
+            iconElement.classList.remove('fa-eye');
           }
     }
 }
