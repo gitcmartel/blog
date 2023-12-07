ce433ac8cd43a06e604e9f82c4aa592c12e3522c
gitcmartel <martel.c73@gmail.com>
2023-12-07 12:03:03 +0100

    Deleting title parameter from function show

--- public/Js/Classes/AlertMessage.js
+++ public/Js/Classes/AlertMessage.js
@@ -1,15 +1,15 @@
 class AlertMessage 
 {
     constructor(){
-        this.toastLiveExample = document.getElementById('toastAlert')
-        this.toast = new bootstrap.Toast(toastLiveExample)
+        this.toastAlert = document.getElementById('toastAlert')
+        this.toast = new bootstrap.Toast(toastAlert)
         this.toastMessage = document.getElementById("toastMessage");
     }
 
     /**
      * Show the alert message
      */
-    showAlertMessage(message)
+    show(message)
     {
         this.toastMessage.innerHTML = message;
         this.toast.show()
