b21c4933f0d4b8103752faea27845017a387fab3
gitcmartel <martel.c73@gmail.com>
2023-12-09 17:47:24 +0100

    Modification of class AlertMessage. Deletion of show method and calling bootstrap toast show method in the class constructor

--- public/Js/Classes/AlertMessage.js
+++ public/Js/Classes/AlertMessage.js
@@ -3,15 +3,6 @@ class AlertMessage
     constructor(){
         this.toastAlert = document.getElementById('toastAlert')
         this.toast = new bootstrap.Toast(toastAlert)
-        this.toastMessage = document.getElementById("toastMessage");
-    }
-
-    /**
-     * Show the alert message
-     */
-    show(message)
-    {
-        this.toastMessage.innerHTML = message;
         this.toast.show()
     }
 }
