8eecd4dc04d17483f523a62e5b3e1f5d48b4e9fd
gitcmartel <martel.c73@gmail.com>
2023-12-13 19:05:02 +0100

    Minor corrections for code lisibility improvements

--- src/Lib/Constants.php
+++ src/Lib/Constants.php
@@ -7,16 +7,16 @@ namespace Application\Lib;
  * Constants
  */
 
-class Constants 
+class Constants
 {
-    CONST WEBSITE_DOMAIN = 'https://blog.devcm.fr/';
-    CONST IMAGE_POST_PATH = 'img\\img-posts\\';
-    CONST DEFAULT_IMAGE_POST = 'blog-post.svg';
-    CONST DEFAULT_IMAGE_POST_PATH = 'img\\blog-post.svg';
-    CONST IMAGE_EXTENSIONS = array("jpeg", "jpg", "png", "svg");
-    CONST IMAGE_MAX_SIZE = 2097152; // 2 Mo
-    CONST NUMBER_OF_COMMENTS_PER_PAGE = 10;
-    CONST NUMBER_OF_POSTS_PER_PAGE = 10;
-    CONST NUMBER_OF_USERS_PER_PAGE = 10;
-    CONST NUMBER_OF_BLOG_POST_PER_PAGE = 4;
+    const WEBSITE_DOMAIN = 'https://blog.devcm.fr/';
+    const IMAGE_POST_PATH = 'img\\img-posts\\';
+    const DEFAULT_IMAGE_POST = 'blog-post.svg';
+    const DEFAULT_IMAGE_POST_PATH = 'img\\blog-post.svg';
+    const IMAGE_EXTENSIONS = ["jpeg", "jpg", "png", "svg"];
+    const IMAGE_MAX_SIZE = 2097152; // 2 Mo
+    const NUMBER_OF_COMMENTS_PER_PAGE = 10;
+    const NUMBER_OF_POSTS_PER_PAGE = 10;
+    const NUMBER_OF_USERS_PER_PAGE = 10;
+    const NUMBER_OF_BLOG_POST_PER_PAGE = 4;
 }
