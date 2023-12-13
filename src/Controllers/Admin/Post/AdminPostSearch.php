0693bfe402ce6f0d605349827abb001a3d3306b0
gitcmartel <martel.c73@gmail.com>
2023-12-13 15:16:19 +0100

    Adding filter_input function to check  and  values.

--- src/Controllers/Admin/Post/AdminPostSearch.php
+++ src/Controllers/Admin/Post/AdminPostSearch.php
@@ -22,25 +22,36 @@ class AdminPostSearch
         #endregion
 
         #region Conditions tests
-        if(! UserActiveCheckValidity::check(array('Administrateur', 'Createur')) || ! isset($_POST['searchString'])
-        || trim($_POST['searchString']) === ""){
+        if(! UserActiveCheckValidity::check(array('Administrateur', 'Createur'))){
             TwigWarning::display(
                 "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site", 
                 "index.php?action=Home\Home", 
                 "Nous contacter");
             return;
         }
-        
+
+        $searchString = filter_input(INPUT_POST, 'searchString', FILTER_SANITIZE_SPECIAL_CHARS);
+
+        //If the searchString variable is not set
+        if ($searchString === false || $searchString === null){
+            TwigWarning::display(
+                "Une erreur est survenue lors du chargement de la page.", 
+                "index.php?action=Home\Home", 
+                "Retour à la page d'accueil");
+            return;
+        }
+
         #endregion
 
         #region Function execution
 
-        $posts = $postRepository->searchPosts(trim($_POST['searchString']));
+        $posts = $postRepository->searchPosts(trim($searchString));
 
         echo $twig->render('Admin\Post\AdminPostList.html.twig', [ 
             'actualPage' => "1", 
             'totalPages' => "1", 
             'posts' => $posts, 
+            'searchString' => $searchString, 
             'activeUser' => Session::getActiveUser(),
             'userFunction' => Session::getActiveUserFunction()
         ]);
