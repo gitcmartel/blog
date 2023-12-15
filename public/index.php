365ec4e2c1208d0ea17d1bab7d80ffa79def15ba
gitcmartel <martel.c73@gmail.com>
2023-12-15 19:24:38 +0100

    Minor style and conditions corrections

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 mb_internal_encoding("UTF-8");
 
-require_once("../vendor/autoload.php");
+require_once "../vendor/autoload.php";
 
 use Application\Controllers\Home\Home;
 use Application\Lib\TwigWarning;
@@ -12,24 +12,24 @@ session_start();
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_URL);
 
 try {
-    if (isset($action) && $action !== ''){
+    if (isset($action) && $action !== '') {
         $class = "Application\\Controllers\\" .
-        preg_replace_callback(
-            '#(?<=\\\\)[a-z]#',
-            function (array $match): string {
-                return mb_strtoupper($match[0]);
-            },
-            $action
-        );
+            preg_replace_callback(
+                '#(?<=\\\\)[a-z]#',
+                function (array $match): string {
+                    return mb_strtoupper($match[0]);
+                },
+                $action
+            );
 
         $classFound = false;
 
-        if (class_exists('\\' . $class)) {
+        if (class_exists('\\' . $class) === true) {
             (new $class())->execute();
             $classFound = true;
         }
 
-        if (! $classFound) {
+        if ($classFound === false) {
             throw new Exception("La page que vous demandez n'existe pas.");
         }
     } else {
@@ -38,10 +38,8 @@ try {
 } catch (Exception $exception) {
     $errorMessage = $exception->getMessage();
     TwigWarning::display(
-        'L\erreur suivante c\'est produite : ' . $errorMessage, 
-        'index.php?action=Home\Home', 
+        'L\erreur suivante c\'est produite : ' . $errorMessage,
+        'index.php?action=Home\Home',
         "Nous contacter"
     );
 }
-
-
