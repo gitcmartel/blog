933f8bd6a1493a970cd58cdd629340b839f3f20e
gitcmartel <martel.c73@gmail.com>
2023-12-15 18:45:26 +0100

    Creation of class AlertModal

---
+++ public/Js/Classes/AlertModal.js
@@ -0,0 +1,13 @@
+class AlertModal {
+  constructor () {
+    this.alertLabel = document.getElementById('confirmationModalLabel');
+    this.alertId = document.getElementById('btnConfirmationModal');
+    this.message = document.getElementById('confirmationMessage');
+  }
+
+  setModal (element) {
+    this.alertLabel.innerHTML = element.getAttribute('data-action');
+    this.alertId.href = element.getAttribute('data-id');
+    this.message.innerHTML = element.getAttribute('data-message');
+  }
+}
