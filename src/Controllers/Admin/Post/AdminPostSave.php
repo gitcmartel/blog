e82d15579e9e508d8c98456d64dacb8db11bd0fc
gitcmartel <martel.c73@gmail.com>
2023-12-16 09:51:57 +0100

    Replacing supervariable  by class File. To secure and verify data before use

--- src/Controllers/Admin/Post/AdminPostSave.php
+++ src/Controllers/Admin/Post/AdminPostSave.php
@@ -7,7 +7,7 @@ use Application\Models\Post;
 use Application\Models\UserRepository;
 use Application\Lib\UserActiveCheckValidity;
 use Application\Lib\Session;
-use Application\Lib\Upload;
+use Application\Lib\File;
 use Application\Lib\TwigLoader;
 use Application\Lib\TwigWarning;
 use Exception;
@@ -22,20 +22,18 @@ class AdminPostSave
         $postRepository = new PostRepository();
         $userRepository = new UserRepository();
         $warningImage = '';
-        $tmpImagePath = '';
-        $imageName = '';
         $twig = TwigLoader::getEnvironment();
 
         #endregion
 
         #region Conditions tests
 
-        if(! UserActiveCheckValidity::check(array('Administrateur', 'Createur'))){
+        if (!UserActiveCheckValidity::check(array('Administrateur', 'Createur'))) {
             TwigWarning::display(
-                "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site", 
-                "index.php?action=Home\Home", 
+                "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site",
+                "index.php?action=Home\Home",
                 "Nous contacter");
-            return;   
+            return;
         }
 
         $postId = filter_input(INPUT_POST, 'postId', FILTER_SANITIZE_NUMBER_INT);
@@ -44,60 +42,62 @@ class AdminPostSave
         $postContent = filter_input(INPUT_POST, 'postContent', FILTER_SANITIZE_SPECIAL_CHARS);
         $resetImage = filter_input(INPUT_POST, 'resetImage', FILTER_VALIDATE_BOOLEAN);
 
-        if($postId === false || $postId === null || $postTitle === false || $postTitle === null || 
-        $postSummary === false || $postSummary === null || $postContent === false || $postContent === null || 
-        $resetImage === null){
-        TwigWarning::display(
-            "Un problème est survenu lors de l'enregistrement du post.", 
-            "index.php?action=Home\Home", 
-            "Retour à la page d'accueil");
-        return;   
+        if (
+            $postId === false || $postId === null || $postTitle === false || $postTitle === null ||
+            $postSummary === false || $postSummary === null || $postContent === false || $postContent === null ||
+            $resetImage === null
+        ) {
+            TwigWarning::display(
+                "Un problème est survenu lors de l'enregistrement du post.",
+                "index.php?action=Home\Home",
+                "Retour à la page d'accueil");
+            return;
         }
 
-        if(isset($_FILES['imagePath']) && $_FILES['imagePath']['error'] === UPLOAD_ERR_OK && $_FILES['imagePath']['size'] > 0){
-            $warningImage = upload::ckeckFile($_FILES['imagePath']['name'], $_FILES['imagePath']['size']);
-            $tmpImagePath = $_FILES["imagePath"]["tmp_name"];
-            $imageName = $_FILES["imagePath"]["name"];
+        $file = new File($_FILES);
+
+        if ($file->validateFile()) {
+            $warningImage = $file->ckeckFile();
         }
 
         $post = new Post();
 
         $post->hydrate(array(
             'id' => intval($postId),
-            'title' => trim($postTitle), 
-            'summary' => trim($postSummary), 
-            'content' => trim($postContent), 
-            'user' => $userRepository->getUser(Session::getActiveUserId()), 
+            'title' => trim($postTitle),
+            'summary' => trim($postSummary),
+            'content' => trim($postContent),
+            'user' => $userRepository->getUser(Session::getActiveUserId()),
             'modifier' => $userRepository->getUser(Session::getActiveUserId())
         ));
 
         $fieldsWarnings = array(
-            'title' => 'Vous devez renseigner un titre', 
-            'summary' => 'Vous devez renseigner un résumé', 
-            'content' => 'Vous devez renseigner un contenu', 
-            'image' => $warningImage, 
+            'title' => 'Vous devez renseigner un titre',
+            'summary' => 'Vous devez renseigner un résumé',
+            'content' => 'Vous devez renseigner un contenu',
+            'image' => $warningImage,
         );
 
-        if($post->getTitle() === "" || $post->getSummary() === "" || $post->getContent() ===""){
-            echo $twig->render('Admin\Post\AdminPost.html.twig', [ 
-                'warningTitle' => $post->getTitle() === '' ? $fieldsWarnings['title'] : '', 
+        if ($post->getTitle() === "" || $post->getSummary() === "" || $post->getContent() === "") {
+            echo $twig->render('Admin\Post\AdminPost.html.twig', [
+                'warningTitle' => $post->getTitle() === '' ? $fieldsWarnings['title'] : '',
                 'warningSummary' => $post->getSummary() === '' ? $fieldsWarnings['summary'] : '',
                 'warningContent' => $post->getContent() === '' ? $fieldsWarnings['content'] : '',
-                'warningImage' => $fieldsWarnings['image'], 
-                'activeUser' => Session::getActiveUser(), 
+                'warningImage' => $fieldsWarnings['image'],
+                'activeUser' => Session::getActiveUser(),
                 'userFunction' => Session::getActiveUserFunction()
             ]);
             return;
         }
 
         $postDatabase = $postRepository->getPost($post->getId());
-       
+
         #endregion
 
         #region Function execution
 
         //If there is a Post Id then we have to make an update
-        if($post->getId() !== 0){ 
+        if ($post->getId() !== 0) {
             //Fetching creationDate and imagePath from the database
             $post->setCreationDate($postDatabase->getCreationDate());
             $post->setImagePath($postDatabase->getImagePath());
@@ -110,22 +110,22 @@ class AdminPostSave
         //Image management function (deletes, update, move physical tmp image etc...)
         try {
             //If we have to reset the image
-            if($resetImage === true){
+            if ($resetImage === true) {
                 $postRepository->resetImage($post);
             }
 
             //If there is a new image we update it
-            if($tmpImagePath !== ''){
-                $postRepository->updateImage($post, $tmpImagePath, $imageName);
+            if ($file->getfilePathTmpName() !== '') {
+                $postRepository->updateImage($post, $file->getfilePathTmpName(), $file->getfilePathName());
             }
 
         } catch (Exception $exception) {
             TwigWarning::display(
                 "Un problème est survenu lors de l'enregistrement de l'image. \n 
-                Desription de l'erreur : " . $exception->getMessage(), 
-                "index.php?action=Admin\Post\AdminPostList", 
+                Desription de l'erreur : " . $exception->getMessage(),
+                "index.php?action=Admin\Post\AdminPostList",
                 "Retour à la liste des posts");
-            return;  
+            return;
         }
 
         //We display the updated post list
