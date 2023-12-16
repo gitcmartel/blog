b062898c69844193122a12222984ac9a83bbb2ee
gitcmartel <martel.c73@gmail.com>
2023-12-16 12:10:43 +0100

    Using filter_input function to check post supervariables before use

--- src/Controllers/Admin/User/AdminUserSave.php
+++ src/Controllers/Admin/User/AdminUserSave.php
@@ -14,6 +14,9 @@ use Application\Lib\Email;
 class AdminUserSave
 {
     #region Functions
+    /**
+     * Controller function execute
+     */
     public function execute()
     {
         #region Variables
@@ -26,61 +29,78 @@ class AdminUserSave
 
         #region Conditions tests
 
-        if(! UserActiveCheckValidity::check(array('Administrateur'))){
-                TwigWarning::display(
-                    "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site", 
-                    "index.php?action=Home\Home", 
-                    "Nous contacter");
-                return; 
+        if (!UserActiveCheckValidity::check(array('Administrateur'))) {
+            TwigWarning::display(
+                "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site",
+                "index.php?action=Home\Home",
+                "Nous contacter");
+            return;
         }
 
-        if (! isset($_POST['userId']) || ! isset($_POST['userName']) || ! isset($_POST['surname']) || ! isset($_POST['pseudo']) || ! isset($_POST['password'])
-        || ! isset($_POST['passwordConfirmation']) || ! isset($_POST['userFunction']) || ! isset($_POST['userValidity'])){
+        $userId = filter_input(INPUT_POST, 'userId', FILTER_SANITIZE_NUMBER_INT);
+        $userName = filter_input(INPUT_POST, 'userName', FILTER_SANITIZE_SPECIAL_CHARS);
+        $userSurname = filter_input(INPUT_POST, 'surname', FILTER_SANITIZE_SPECIAL_CHARS);
+        $pseudo = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_SPECIAL_CHARS);
+        $userMail = filter_input(INPUT_POST, 'userMail', FILTER_VALIDATE_EMAIL);
+        $password = filter_input(INPUT_POST, 'password', FILTER_UNSAFE_RAW);
+        $passwordConfirmation = filter_input(INPUT_POST, 'passwordConfirmation', FILTER_UNSAFE_RAW);
+        $userFunction = filter_input(INPUT_POST, 'userFunction', FILTER_SANITIZE_SPECIAL_CHARS);
+        $userValidity = filter_input(INPUT_POST, 'userValidity', FILTER_VALIDATE_BOOLEAN);
+        $passwordChange = filter_input(INPUT_POST, 'passwordChange', FILTER_VALIDATE_BOOLEAN);
+
+        if (
+            $userId === false || $userId === null || $userName === false || $userName === null || $userSurname === false || $userSurname === null ||
+            $pseudo === false || $pseudo === null || $userMail === false || $userMail === null || $password === false || $password === null ||
+            $passwordConfirmation === false || $passwordConfirmation === null ||
+            $userFunction === false || $userFunction === null || $userValidity === null
+        ) {
             TwigWarning::display(
-                "Une erreur est survenue lors du chargement de la page.", 
-                "index.php?action=Home\Home", 
+                "Une erreur est survenue lors du chargement de la page.",
+                "index.php?action=Home\Home",
                 "Retour à la page d'accueil");
             return;
         }
 
         $user = new User();
-        
+
         $user->hydrate(array(
-            'id' => trim($_POST["userId"]) === '' ? null : intval(trim($_POST["userId"])),
-            'name' => trim($_POST['userName']), 
-            'surname' => trim($_POST['surname']), 
-            'pseudo' => trim($_POST['pseudo']), 
-            'email' => trim($_POST['userMail']),
-            'userFunction' => trim($_POST['userFunction']), 
-            'isValid' => trim($_POST['userValidity']),
-            'password' => trim($_POST['password']),
+            'id' => intval($userId),
+            'name' => trim($userName),
+            'surname' => trim($userSurname),
+            'pseudo' => trim($pseudo),
+            'email' => trim($userMail),
+            'userFunction' => trim($userFunction),
+            'isValid' => trim($userValidity),
+            'password' => trim($password),
         ));
 
-        $passwordConfirmation = trim($_POST['passwordConfirmation']);
+        $passwordConfirmation = trim($passwordConfirmation);
 
         $fieldsWarnings = array(
-            'name' => 'Vous devez renseigner un prénom', 
-            'surname' => 'Vous devez renseigner un nom', 
-            'pseudo' => 'Vous devez renseigner un pseudo', 
-            'email' => Email::checkMailFormat($user->getEmail()) ? '' : 'L\'adresse email est incorrecte',  
-            'userFunction' => 'Vous devez renseigner une fonction', 
-            'password' => isset($_POST['passwordChange']) ? Password::checkPasswordFormFields($user->getPassword(), $passwordConfirmation) : '', 
+            'name' => 'Vous devez renseigner un prénom',
+            'surname' => 'Vous devez renseigner un nom',
+            'pseudo' => 'Vous devez renseigner un pseudo',
+            'email' => Email::checkMailFormat($user->getEmail()) ? '' : 'L\'adresse email est incorrecte',
+            'userFunction' => 'Vous devez renseigner une fonction',
+            'password' => $passwordChange === true ? Password::checkPasswordFormFields($user->getPassword(), $passwordConfirmation) : '',
             'validity' => 'Vous devez selectionner une option'
         );
 
         //If there is an incorrect field we display the error message
-        if($user->getName() === "" || $user->getSurname() === "" || $user->getPseudo() === "" || $user->getEmail() === "" || 
-        $user->getUserFunction() === "" || $user->getIsValid() === "" || $fieldsWarnings['email'] !== '' || $fieldsWarnings['password'] !== ''){
-            echo $twig->render('Admin\User\AdminUser.html.twig', [ 
-                'warningName' => $user->getName() === "" ? $fieldsWarnings['name'] : '', 
-                'warningSurname' => $user->getSurname() === "" ? $fieldsWarnings['surname'] : '', 
-                'warningPseudo' => $user->getPseudo() === "" ? $fieldsWarnings['pseudo'] : '', 
-                'warningEmail' => $fieldsWarnings['email'], 
-                'warningPassword' => $fieldsWarnings['password'], 
-                'warningFunction' => $user->getUserFunction() === "" ? $fieldsWarnings['userFunction'] : '', 
-                'warningValidity' => $user->getIsValid() === "" ? $fieldsWarnings['validity'] : '',  
+        if (
+            $user->getName() === "" || $user->getSurname() === "" || $user->getPseudo() === "" || $user->getEmail() === "" ||
+            $user->getUserFunction() === "" || $user->getIsValid() === "" || $fieldsWarnings['email'] !== '' || $fieldsWarnings['password'] !== ''
+        ) {
+            echo $twig->render('Admin\User\AdminUser.html.twig', [
+                'warningName' => $user->getName() === "" ? $fieldsWarnings['name'] : '',
+                'warningSurname' => $user->getSurname() === "" ? $fieldsWarnings['surname'] : '',
+                'warningPseudo' => $user->getPseudo() === "" ? $fieldsWarnings['pseudo'] : '',
+                'warningEmail' => $fieldsWarnings['email'],
+                'warningPassword' => $fieldsWarnings['password'],
+                'warningFunction' => $user->getUserFunction() === "" ? $fieldsWarnings['userFunction'] : '',
+                'warningValidity' => $user->getIsValid() === "" ? $fieldsWarnings['validity'] : '',
                 'user' => $user,
-                'pwdChangeCheckedValue' => isset($_POST['passwordChange']) ? 'checked' : '',
+                'pwdChangeCheckedValue' => $passwordChange === true ? 'checked' : '',
                 'userFunction' => Session::getActiveUserFunction(),
                 'activeUser' => Session::getActiveUser()
             ]);
@@ -90,8 +110,8 @@ class AdminUserSave
         #endregion
 
         #region Function execution
-        
-        if ($user->getId() !== null){
+
+        if ($user->getId() !== null) {
             //If there is a userId we update
             if ($userRepository->updateUser($user)) {
                 //We display the updated user list
@@ -99,24 +119,24 @@ class AdminUserSave
                 return;
             } else {
                 TwigWarning::display(
-                    "Un problème est survenu lors de l'enregistrement de l'utilisateur.", 
-                    "index.php?action=Home\Home", 
+                    "Un problème est survenu lors de l'enregistrement de l'utilisateur.",
+                    "index.php?action=Home\Home",
                     "Retour à l'accueil");
-                return; 
+                return;
             }
-        }  
+        }
 
         //If there is no userId we create a new user
-        if ($userRepository->createUser($user)){
+        if ($userRepository->createUser($user)) {
             //We display the updated user list
             header("Location:index.php?action=Admin\User\AdminUserList&pageNumber=1");
             return;
         } else {
             TwigWarning::display(
-                "Un problème est survenu lors de l'enregistrement de l'utilisateur.", 
-                "index.php?action=Home\Home", 
+                "Un problème est survenu lors de l'enregistrement de l'utilisateur.",
+                "index.php?action=Home\Home",
                 "Retour à l'accueil");
-            return; 
+            return;
         }
 
         #endregion
