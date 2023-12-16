f4332638c1be6bcd3736e2bf4b43822990b09716
gitcmartel <martel.c73@gmail.com>
2023-12-16 09:50:09 +0100

    Minor code style modifications

--- src/Lib/Image.php
+++ src/Lib/Image.php
@@ -3,17 +3,18 @@
 namespace Application\Lib;
 
 use DateTime;
-class Image 
+
+class Image
 {
     #region Functions 
 
     /**
      * Move the image file from the temp server folder, to the the image post folder
      */
-    public static function moveTempImageIntoImagePostFolder(string $tmpImagePath, string $destinationPath) : bool
+    public static function moveTempImageIntoImagePostFolder(string $tmpImagePath, string $destinationPath): bool
     {
-        if (! move_uploaded_file($tmpImagePath, dirname(__FILE__, 3) . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . $destinationPath)) {
-            return false;  
+        if (!move_uploaded_file($tmpImagePath, dirname(__FILE__, 3) . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . $destinationPath)) {
+            return false;
         }
         return true;
     }
@@ -22,15 +23,15 @@ class Image
     {
         //If the image post is not the default image we delete it
         $rootPath = dirname(__FILE__, 3) . DIRECTORY_SEPARATOR . 'public\\';
-        
-        if ($imagePath !== Path::fileBuildPath(array("img", Constants::DEFAULT_IMAGE_POST)) && $imagePath !== ''){
-            if (file_exists($rootPath . $imagePath)){
+
+        if ($imagePath !== Path::fileBuildPath(array("img", Constants::DEFAULT_IMAGE_POST)) && $imagePath !== '') {
+            if (file_exists($rootPath . $imagePath)) {
                 return unlink($rootPath . $imagePath);
             }
         }
     }
 
-    public static function createImagePathName(int $postId, string $imageName, DateTime $date) : string
+    public static function createImagePathName(int $postId, string $imageName, DateTime $date): string
     {
         return $postId . $date->format("YmdHis") . '.' . File::getExtension($imageName);
     }
