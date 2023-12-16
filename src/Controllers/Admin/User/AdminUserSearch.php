448399a0aa91da2d508d3c3d8592b7958e570989
gitcmartel <martel.c73@gmail.com>
2023-12-16 10:10:00 +0100

    Using function filter_input to check post supervariable before use

--- src/Controllers/Admin/User/AdminUserSearch.php
+++ src/Controllers/Admin/User/AdminUserSearch.php
@@ -4,7 +4,6 @@ namespace Application\Controllers\Admin\User;
 
 use Application\Models\UserRepository;
 use Application\Lib\UserActiveCheckValidity;
-use Application\Lib\DatabaseConnexion;
 use Application\Lib\Session;
 use Application\Lib\TwigLoader;
 use Application\Lib\TwigWarning;
@@ -23,7 +22,7 @@ class AdminUserSearch
 
         #region Conditions tests
 
-        if(! UserActiveCheckValidity::check(array('Administrateur')) || ! isset($_POST['searchString'])){
+        if(! UserActiveCheckValidity::check(array('Administrateur'))){
             TwigWarning::display(
                 "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site", 
                 "index.php?action=Home\Home", 
@@ -31,16 +30,28 @@ class AdminUserSearch
             return; 
         }
 
+        $searchString = filter_input(INPUT_POST, 'searchString', FILTER_SANITIZE_SPECIAL_CHARS);
+
+        //If the searchString variable is not set
+        if ($searchString === false || $searchString === null){
+            TwigWarning::display(
+                "Une erreur est survenue lors du chargement de la page.", 
+                "index.php?action=Home\Home", 
+                "Retour à la page d'accueil");
+            return;
+        }
+
         #endregion
 
         #region Function execution
         
-        $users = $userRepository->searchUsers(trim($_POST['searchString']));
+        $users = $userRepository->searchUsers(trim($searchString));
 
         echo $twig->render('Admin\User\AdminUserList.html.twig', [ 
             'actualPage' => "1", 
             'totalPages' => "1", 
             'users' => $users, 
+            'searchString' => $searchString, 
             'userFunction' => Session::getActiveUserFunction(),
             'activeUser' => Session::getActiveUser()
         ]);
