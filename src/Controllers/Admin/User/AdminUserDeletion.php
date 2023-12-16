b04afaeb20cc538b0793202cbe67066fc935fed5
gitcmartel <martel.c73@gmail.com>
2023-12-16 11:27:32 +0100

    Minor code style modifications

--- src/Controllers/Admin/User/AdminUserDeletion.php
+++ src/Controllers/Admin/User/AdminUserDeletion.php
@@ -19,22 +19,22 @@ class AdminUserDeletion
         #endregion
 
         #region Conditions tests
-        
-        if(! UserActiveCheckValidity::check(array('Administrateur'))){
+
+        if (!UserActiveCheckValidity::check(array('Administrateur'))) {
             TwigWarning::display(
-                "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site", 
-                "index.php?action=Home\Home", 
+                "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site",
+                "index.php?action=Home\Home",
                 "Nous contacter");
-            return; 
+            return;
         }
 
         $userId = filter_input(INPUT_GET, 'userId', FILTER_SANITIZE_NUMBER_INT);
 
         //If the commentId variable is not set
-        if ($userId === false || $userId === null){
+        if ($userId === false || $userId === null) {
             TwigWarning::display(
-                "Une erreur est survenue lors du chargement de la page.", 
-                "index.php?action=Home\Home", 
+                "Une erreur est survenue lors du chargement de la page.",
+                "index.php?action=Home\Home",
                 "Retour à la page d'accueil");
             return;
         }
@@ -42,10 +42,10 @@ class AdminUserDeletion
         $user = $userRepository->getUser($userId);
 
         //If the user id is null
-        if($user->getid() === null){
+        if ($user->getid() === null) {
             TwigWarning::display(
-                "Une erreur est survenue lors de la suppression de l'utilisateur.", 
-                "index.php?action=Home\Home", 
+                "Une erreur est survenue lors de la suppression de l'utilisateur.",
+                "index.php?action=Home\Home",
                 "Retour à la page d'accueil");
             return;
         }
@@ -54,16 +54,16 @@ class AdminUserDeletion
 
 
         #region Function execution
-                    
 
 
-        if (! $userRepository->deleteUser($user->getId())) {
+
+        if (!$userRepository->deleteUser($user->getId())) {
             TwigWarning::display(
-                "Un problème est survenu lors de la suppression de l'utilisateur.", 
-                "index.php?action=Home\Home", 
+                "Un problème est survenu lors de la suppression de l'utilisateur.",
+                "index.php?action=Home\Home",
                 "Retour à l'accueil");
-            return; 
-        } else  {
+            return;
+        } else {
             //We display the updated user list
             header("Location:index.php?action=Admin\User\AdminUserList&pageNumber=1&alert=true&alertType=Deletion");
             return;
