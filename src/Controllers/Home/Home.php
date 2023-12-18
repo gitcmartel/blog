8dd8907775b2dd7bb1a5b893f5e3f71d6ba1dd7a
gitcmartel <martel.c73@gmail.com>
2023-12-18 20:01:12 +0100

    Replacing direct use of post and get supervariables by filter_input function

--- src/Controllers/Home/Home.php
+++ src/Controllers/Home/Home.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Application\Controllers\Home;
 
@@ -9,10 +9,11 @@ use Application\Lib\Session;
 use Application\Lib\TwigLoader;
 use Application\Lib\Alert;
 
-class Home 
+class Home
 {
     #region Functions
-    public function execute() {
+    public function execute()
+    {
 
         #region Variables
         $postRepository = new PostRepository();
@@ -26,29 +27,39 @@ class Home
         //Get the latest 3 posts
         $posts = $postRepository->getPosts(1, 3, true);
 
+        $alert = filter_input(INPUT_GET, 'alert', FILTER_SANITIZE_SPECIAL_CHARS);
+        $alertType = filter_input(INPUT_GET, 'alertType', FILTER_SANITIZE_SPECIAL_CHARS);
+
+        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+        $surname = filter_input(INPUT_POST, 'surname', FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_SPECIAL_CHARS);
+        $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
+
         //If the fields are not set we simply display the home page
-        if (! isset($_POST['surname']) && ! isset($_POST['name']) && ! isset($_POST['email']) && ! isset($_POST['message'])){
-            echo $twig->render('Home\Home.html.twig', [ 
-                'messageResponse' => "", 
-                'posts' => $posts, 
-                'alert' => isset($_GET['alert']) ? $_GET['alert'] : '',
-                'alertMessage' => isset($_GET['alertType']) ? Alert::getMessage($_GET['alertType']) : '',
-                'activeUser' => Session::getActiveUser(), 
+        if (
+            $name === false || $name === null || $surname === false || $surname === null ||
+            $email === false || $email === null || $message === false || $message === null
+        ) {
+            echo $twig->render('Home\Home.html.twig', [
+                'messageResponse' => "",
+                'posts' => $posts,
+                'alert' => ($alert !== false && $alert !== null) ? $alert : '',
+                'alertMessage' => ($alertType !== false && $alertType !== null) ? Alert::getMessage($alertType) : '',
+                'activeUser' => Session::getActiveUser(),
                 'userFunction' => Session::getActiveUserFunction()
             ]);
             return;
         }
 
         //All the form fields have to be filled
-        if (trim($_POST['surname']) === "" || trim($_POST['name']) === "" || trim($_POST['email']) === "" 
-        || trim($_POST['message']) === "" ){
-            echo $twig->render('Home\Home.html.twig', [ 
-                'warningMessage' => "Vous devez compléter tous les champs du formulaire !", 
-                'surname' => $_POST['surname'], 
-                'name' => $_POST['name'], 
-                'email' => $_POST['email'], 
-                'message' => $_POST['message'], 
-                'activeUser' => Session::getActiveUser(), 
+        if (trim($surname) === "" || trim($name) === "" || trim($email) === "" || trim($message) === "") {
+            echo $twig->render('Home\Home.html.twig', [
+                'warningMessage' => "Vous devez compléter tous les champs du formulaire !",
+                'surname' => $surname,
+                'name' => $name,
+                'email' => $email,
+                'message' => $message,
+                'activeUser' => Session::getActiveUser(),
                 'userFunction' => Session::getActiveUserFunction()
             ]);
             return;
@@ -56,20 +67,20 @@ class Home
 
         //Checks the validity of the form fields data
         $formValidation = new FormValidation(
-            $_POST['surname'], 
-            $_POST['name'], 
-            $_POST['email'], 
-            $_POST['message']
+            $surname,
+            $name,
+            $email,
+            $message
         );
 
-        if (! $formValidation->isValid()) {
-            echo $twig->render('Home\Home.html.twig', [ 
-                'warningMessage' => "Un ou plusieurs champs du formulaire sont invalides !", 
-                'surname' => $_POST['surname'], 
-                'name' => $_POST['name'], 
-                'email' => $_POST['email'], 
-                'message' => $_POST['message'], 
-                'activeUser' => Session::getActiveUser(), 
+        if (!$formValidation->isValid()) {
+            echo $twig->render('Home\Home.html.twig', [
+                'warningMessage' => "Un ou plusieurs champs du formulaire sont invalides !",
+                'surname' => $surname,
+                'name' => $name,
+                'email' => $email,
+                'message' => $message,
+                'activeUser' => Session::getActiveUser(),
                 'userFunction' => Session::getActiveUserFunction()
             ]);
             return;
@@ -80,28 +91,28 @@ class Home
         #region Function execution
 
         $mail = new Email(
-            $_POST['surname'], 
-            $_POST['name'],
+            $surname,
+            $name,
             "contact@blog.devcm.fr",
-            "contact@blog.devcm.fr",  
-            "Nouveau contact !", 
-            "Message envoyé depuis le formulaire de contact du Blog Devcm. \r\n" . 
-            "Auteur : " . $_POST['surname'] . " " . $_POST['name'] . " - " . $_POST['email'] . "\r\n" . 
-            $_POST['message']
+            "contact@blog.devcm.fr",
+            "Nouveau contact !",
+            "Message envoyé depuis le formulaire de contact du Blog Devcm. \r\n" .
+            "Auteur : " . $surname . " " . $name . " - " . $email . "\r\n" .
+            $message
         );
 
-        if($mail->sendMail()) {
+        if ($mail->sendMail()) {
             $messageResponse = 'Le message a été envoyé.';
         } else {
             $messageResponse = 'Erreur de Mailer : ' . $mail->getErrorInfo();
         }
 
-        echo $twig->render('Home\Home.html.twig', [ 
-            'messageResponse' => $messageResponse, 
+        echo $twig->render('Home\Home.html.twig', [
+            'messageResponse' => $messageResponse,
             'alert' => true,
             'alertMessage' => $messageResponse,
-            'posts' => $posts, 
-            'activeUser' => Session::getActiveUser(), 
+            'posts' => $posts,
+            'activeUser' => Session::getActiveUser(),
             'userFunction' => Session::getActiveUserFunction()
         ]);
 
