eeec026cfea97b1fcab52ea58d8dc3d22706515c
gitcmartel <martel.c73@gmail.com>
2023-12-18 19:40:05 +0100

    Adding setUser function

--- src/Lib/Session.php
+++ src/Lib/Session.php
@@ -2,6 +2,8 @@
 
 namespace Application\Lib;
 
+use Application\Models\User;
+
 class Session
 {
     #region Functions
@@ -9,33 +11,51 @@ class Session
     /**
      * Returns the user pseudo If it is registered in the superglobal $_SESSION
      * returns an empty string if not
+     * @return string
      */
-    public static function getActiveUser() : string
+    public static function getActiveUser(): string
     {
-        if (isset($_SESSION['activeUser'])){
+        if (isset($_SESSION['activeUser'])) {
             return $_SESSION['activeUser'];
         } else {
             return "";
         }
     }
 
-    public static function getActiveUserFunction() : string
+    /**
+     * Returns the active user's function
+     * @return string
+     */
+    public static function getActiveUserFunction(): string
     {
-        if (isset($_SESSION['activeUserFunction'])){
+        if (isset($_SESSION['activeUserFunction'])) {
             return $_SESSION['activeUserFunction'];
         } else {
             return "";
         }
     }
 
-    public static function getActiveUserId() : ?int
+    /**
+     * Returns the active user's id
+     * @return int
+     */
+    public static function getActiveUserId(): ?int
     {
-        if (isset($_SESSION['userId'])){
+        if (isset($_SESSION['userId'])) {
             return $_SESSION['userId'];
         } else {
             return null;
         }
     }
 
+    /**
+     * Set the active user data into the $_SESSION supervariable
+     */
+    public static function setUser(User $user): void
+    {
+        $_SESSION['activeUser'] = $user->getPseudo();
+        $_SESSION['activeUserFunction'] = $user->getUserFunction()->toString();
+        $_SESSION['userId'] = $user->getId();
+    }
     #endregion
 }
