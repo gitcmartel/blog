1ea9106ac0747f32d418381ee8369122ce8b0952
gitcmartel <martel.c73@gmail.com>
2023-12-19 20:29:40 +0100

    Using filter_input function to replace direct use of get and post supervariables.

--- src/Controllers/UserAccount/UserAccount.php
+++ src/Controllers/UserAccount/UserAccount.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 
 namespace Application\Controllers\UserAccount;
@@ -11,68 +11,81 @@ use Application\Lib\TwigWarning;
 use Application\Lib\Email;
 use Application\Lib\Pseudo;
 
-class UserAccount 
+class UserAccount
 {
     #region Functions
+    /**
+     * Controller main function
+     */
     public function execute()
     {
         #region Variables
 
         $twig = TwigLoader::getEnvironment();
         $userRepository = new UserRepository();
-        
+
         #endregion 
 
         #region Conditions tests
-        
+        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+        $surname = filter_input(INPUT_POST, 'surname', FILTER_SANITIZE_SPECIAL_CHARS);
+        $pseudo = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        $password = filter_input(INPUT_POST, 'password', FILTER_DEFAULT);
+        $passwordConfirmation = filter_input(INPUT_POST, 'passwordConfirmation', FILTER_DEFAULT);
+        $passwordChange = filter_input(INPUT_POST, 'passwordChange', FILTER_VALIDATE_BOOLEAN);
+
         //Checks if an account update has been sent
-        if (! (isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['pseudo']) && isset($_POST['email']) 
-        && isset($_POST['password']) && isset($_POST['passwordConfirmation']))) {
+        if (
+            $name === false || $name === null || $surname === false || $surname === null ||
+            $pseudo === false || $pseudo === null || $email === false || $email === null ||
+            $password === false || $password === null || $passwordConfirmation === false || $passwordConfirmation === null
+        ) {
 
             $user = $userRepository->getUser(Session::getActiveUserId());
 
             echo $twig->render('UserAccount\UserAccount.html.twig', [
-                'user' => $user, 
-                'activeUser' => Session::getActiveUser(), 
+                'user' => $user,
+                'activeUser' => Session::getActiveUser(),
                 'userFunction' => Session::getActiveUserFunction()
-            ]);  
-            return; 
+            ]);
+            return;
         }
 
         $user = $userRepository->getUser(Session::getActiveUserId());
 
-        $passwordConfirmation = trim($_POST['passwordConfirmation']);
-        
         $fieldsWarnings = array(
-            'warningName' => ! $userRepository->checkNameSurname($_POST['name']) ? 'Le champ prénom doit être complété (50 caractères max)' : '',
-            'warningSurname' => ! $userRepository->checkNameSurname($_POST['surname']) ? 'Le champ nom doit être complété (50 caractères max)' : '',
-            'warningEmail' => Email::checkMailFormat($_POST['email']) ? '' : 'L\'adresse email est incorrecte',  
-            'warningPseudo' => $user->getPseudo() !== trim($_POST['pseudo']) ? Pseudo::checkPseudo(trim($_POST['pseudo'])) : '', 
-            'warningPassword' => isset($_POST['passwordChange']) ? Password::checkPasswordFormFields(trim($_POST['password']), $passwordConfirmation) : ''
+            'warningName' => !$userRepository->checkNameSurname($name) ? 'Le champ prénom doit être complété (50 caractères max)' : '',
+            'warningSurname' => !$userRepository->checkNameSurname($surname) ? 'Le champ nom doit être complété (50 caractères max)' : '',
+            'warningEmail' => Email::checkMailFormat($email) ? '' : 'L\'adresse email est incorrecte',
+            'warningPseudo' => $user->getPseudo() !== trim($pseudo) ? Pseudo::checkPseudo(trim($pseudo)) : '',
+            'warningPassword' => $passwordChange === true ? Password::checkPasswordFormFields(trim($password), $passwordConfirmation) : ''
         );
 
-        $user->hydrate(array (
-            "name"=> trim($_POST["name"]),
-            "surname"=> trim($_POST["surname"]),
-            "pseudo"=> trim($_POST["pseudo"]),
-            "email"=> trim($_POST["email"]),
-            "password"=> isset($_POST['passwordChange']) ? $_POST["password"] : ""
+        $user->hydrate(array(
+            "name" => trim($name),
+            "surname" => trim($surname),
+            "pseudo" => trim($pseudo),
+            "email" => trim($email),
+            "password" => $passwordChange === true ? $password : ""
         ));
 
         //If there is a warning to display (incorrect field content)
-        if (! ($fieldsWarnings["warningName"] === "" && $fieldsWarnings["warningSurname"] === "" && $fieldsWarnings["warningEmail"] === "" 
-        && $fieldsWarnings["warningPseudo"] === "" && $fieldsWarnings["warningPassword"] === "")){
+        if (
+            !($fieldsWarnings["warningName"] === "" && $fieldsWarnings["warningSurname"] === "" && $fieldsWarnings["warningEmail"] === ""
+                && $fieldsWarnings["warningPseudo"] === "" && $fieldsWarnings["warningPassword"] === "")
+        ) {
             echo $twig->render('UserAccount\UserAccount.html.twig', [
-                'warningName' => $fieldsWarnings["warningName"], 
-                'warningSurname' => $fieldsWarnings["warningSurname"], 
-                'warningEmail' => $fieldsWarnings["warningEmail"], 
-                'warningPseudo' => $fieldsWarnings["warningPseudo"], 
-                'warningPassword' => isset($_POST['passwordChange']) ? Password::checkPasswordFormFields($user->getPassword(), $passwordConfirmation) : '',  
-                'pwdChangeCheckedValue' => isset($_POST['passwordChange']) ? 'checked' : '',
-                'user' => $user, 
-                'activeUser' => Session::getActiveUser(), 
+                'warningName' => $fieldsWarnings["warningName"],
+                'warningSurname' => $fieldsWarnings["warningSurname"],
+                'warningEmail' => $fieldsWarnings["warningEmail"],
+                'warningPseudo' => $fieldsWarnings["warningPseudo"],
+                'warningPassword' => $passwordChange === true ? Password::checkPasswordFormFields($user->getPassword(), $passwordConfirmation) : '',
+                'pwdChangeCheckedValue' => $passwordChange === true ? 'checked' : '',
+                'user' => $user,
+                'activeUser' => Session::getActiveUser(),
                 'userFunction' => Session::getActiveUserFunction()
-            ]);  
+            ]);
             return;
         }
 
@@ -80,12 +93,12 @@ class UserAccount
 
         #region Function execution
 
-        if($userRepository->updateUser($user)){
+        if ($userRepository->updateUser($user)) {
             header("Location:index.php?action=Home\Home");
         } else {
             TwigWarning::display(
-                "Un problème est survenu lors de la mise à jour de votre compte.", 
-                "index.php?action=UserAccount\UserAccount", 
+                "Un problème est survenu lors de la mise à jour de votre compte.",
+                "index.php?action=UserAccount\UserAccount",
                 "Réessayer");
         }
 
