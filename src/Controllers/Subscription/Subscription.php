bdd9f67fcc1d3558a12788e10afee2c9964500af
gitcmartel <martel.c73@gmail.com>
2023-12-19 19:42:30 +0100

    Using filter_input function to replace direct use of get and post supervariables. Replacing success page by alert message in case of success.

--- src/Controllers/Subscription/Subscription.php
+++ src/Controllers/Subscription/Subscription.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 
 namespace Application\Controllers\Subscription;
@@ -12,69 +12,84 @@ use Application\Lib\TwigWarning;
 use Application\Lib\Email;
 use Application\Lib\Pseudo;
 
-class Subscription 
+class Subscription
 {
     #region Functions
+    /**
+     * Controller main function
+     */
     public function execute()
     {
         #region Variables
 
         $twig = TwigLoader::getEnvironment();
         $userRepository = new UserRepository();
-        
+
         #endregion 
 
         #region Conditions tests
-        
+
+        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+        $surname = filter_input(INPUT_POST, 'surname', FILTER_SANITIZE_SPECIAL_CHARS);
+        $pseudo = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        $password = filter_input(INPUT_POST, 'password', FILTER_DEFAULT);
+        $passwordConfirmation = filter_input(INPUT_POST, 'passwordConfirmation', FILTER_DEFAULT);
+
         //Checks if a new account creation has been sent
-        if (! (isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['pseudo']) && isset($_POST['email']) 
-        && isset($_POST['password']) && isset($_POST['passwordConfirmation']))) {
+        if (
+            $name === false || $name === null || $surname === false || $surname === null ||
+            $pseudo === false || $pseudo === null || $email === false || $email === null ||
+            $password === false || $password === null || $passwordConfirmation === false || $passwordConfirmation === null
+        ) {
             echo $twig->render('Subscription\Subscription.html.twig', [
-                'warningName' => '', 
-                'warningSurname' => '', 
-                'warningEmail' => '', 
-                'warningPseudo' => '', 
-                'warningPassword' => '', 
-                'activeUser' => Session::getActiveUser(), 
+                'warningName' => '',
+                'warningSurname' => '',
+                'warningEmail' => '',
+                'warningPseudo' => '',
+                'warningPassword' => '',
+                'activeUser' => Session::getActiveUser(),
                 'userFunction' => Session::getActiveUserFunction()
-            ]);  
-            return; 
+            ]);
+            return;
         }
 
         $user = new User();
 
-        $user->hydrate(array (
-            "name"=> $_POST["name"],
-            "surname"=> $_POST["surname"],
-            "pseudo"=> $_POST["pseudo"],
-            "email"=> $_POST["email"],
-            "password"=> $_POST["password"], 
-            "creationDate"=> Date("Y-m-d H:i:s"), 
-            "userFunction"=> 'Lecteur', 
-            'isValid'=> 0,
+        $user->hydrate(array(
+            "name" => $name,
+            "surname" => $surname,
+            "pseudo" => $pseudo,
+            "email" => $email,
+            "password" => $password,
+            "creationDate" => Date("Y-m-d H:i:s"),
+            "userFunction" => 'Lecteur',
+            'isValid' => 0,
         ));
 
         $fieldsWarnings = array(
-            'warningName' => ! $userRepository->checkNameSurname($_POST['name']) ? 'Le champ prénom doit être complété (50 caractères max)' : '',
-            'warningSurname' => ! $userRepository->checkNameSurname($_POST['surname']) ? 'Le champ prénom doit être complété (50 caractères max)' : '',
-            'warningEmail' => Email::checkMailFormat($_POST['email']) ? '' : 'L\'adresse email est incorrecte',  
-            'warningPseudo' => Pseudo::checkPseudo($_POST['pseudo']), 
-            'warningPassword' => Password::checkPasswordFormFields($_POST['password'], $_POST['passwordConfirmation'], 0)
+            'warningName' => !$userRepository->checkNameSurname($name) ? 'Le champ prénom doit être complété (50 caractères max)' : '',
+            'warningSurname' => !$userRepository->checkNameSurname($surname) ? 'Le champ prénom doit être complété (50 caractères max)' : '',
+            'warningEmail' => Email::checkMailFormat($email) ? '' : 'L\'adresse email est incorrecte',
+            'warningPseudo' => Pseudo::checkPseudo($pseudo),
+            'warningPassword' => Password::checkPasswordFormFields($password, $passwordConfirmation)
         );
 
         //If there is a warning to display (incorrect field content)
-        if (! ($fieldsWarnings["warningName"] === "" && $fieldsWarnings["warningSurname"] === "" && $fieldsWarnings["warningEmail"] === "" 
-        && $fieldsWarnings["warningPseudo"] === "" && $fieldsWarnings["warningPassword"] === "")){
+        if (
+            !($fieldsWarnings["warningName"] === "" && $fieldsWarnings["warningSurname"] === "" && $fieldsWarnings["warningEmail"] === ""
+                && $fieldsWarnings["warningPseudo"] === "" && $fieldsWarnings["warningPassword"] === "")
+        ) {
             echo $twig->render('Subscription\Subscription.html.twig', [
-                'warningName' => $fieldsWarnings["warningName"], 
-                'warningSurname' => $fieldsWarnings["warningSurname"], 
-                'warningEmail' => $fieldsWarnings["warningEmail"], 
-                'warningPseudo' => $fieldsWarnings["warningPseudo"], 
-                'warningPassword' => $fieldsWarnings["warningPassword"], 
-                'user' => $user, 
-                'activeUser' => Session::getActiveUser(), 
+                'warningName' => $fieldsWarnings["warningName"],
+                'warningSurname' => $fieldsWarnings["warningSurname"],
+                'warningEmail' => $fieldsWarnings["warningEmail"],
+                'warningPseudo' => $fieldsWarnings["warningPseudo"],
+                'warningPassword' => $fieldsWarnings["warningPassword"],
+                'user' => $user,
+                'activeUser' => Session::getActiveUser(),
                 'userFunction' => Session::getActiveUserFunction()
-            ]);  
+            ]);
             return;
         }
 
@@ -82,14 +97,12 @@ class Subscription
 
         #region Function execution
 
-        if($userRepository->createUser($user)){
-            echo $twig->render('Subscription\SubscriptionSuccess.html.twig', [
-                'activeUser' => Session::getActiveUser()
-            ]);  
+        if ($userRepository->createUser($user)) {
+            header("Location:index.php?action=Home\Home&alert=true&alertType=SubscriptionSuccess");
         } else {
             TwigWarning::display(
-                "Un problème est survenu lors de la création du compte.", 
-                "index.php?action=Subscription\Subscription", 
+                "Un problème est survenu lors de la création du compte.",
+                "index.php?action=Subscription\Subscription",
                 "Réessayer");
         }
 
