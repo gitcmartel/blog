df38b0fef770f3e00759d2f9da101ad81f904dfb
gitcmartel <martel.c73@gmail.com>
2023-12-21 15:00:20 +0100

    Minor code style corrections and comments adding

--- src/Controllers/Admin/Comment/AdminCommentSave.php
+++ src/Controllers/Admin/Comment/AdminCommentSave.php
@@ -14,7 +14,10 @@ use Application\Lib\TwigWarning;
 class AdminCommentSave
 {
     #region Functions
-    public function execute()
+    /**
+     * Controller main function
+     */
+    public function execute(): void
     {
         #region variables
         $commentRepository = new CommentRepository();
@@ -26,10 +29,10 @@ class AdminCommentSave
         #region Conditions tests
 
         //If the active user is not an admin
-        if(! UserActiveCheckValidity::check(array('Administrateur'))){
+        if (UserActiveCheckValidity::check(array('Administrateur')) === false) {
             TwigWarning::display(
-                "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site", 
-                "index.php?action=Home\Home", 
+                "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site",
+                "index.php?action=Home\Home",
                 "Nous contacter");
             return;
         }
@@ -40,37 +43,39 @@ class AdminCommentSave
         $validation = filter_input(INPUT_POST, 'validation', FILTER_SANITIZE_SPECIAL_CHARS);
 
         //If the commentId variable is not set
-        if ($postId === false || $postId === null || $commentId === false || $commentId === null || $commentString === false 
-        || $commentString === null){
+        if (
+            $postId === false || $postId === null || $commentId === false || $commentId === null || $commentString === false
+            || $commentString === null
+        ) {
             TwigWarning::display(
-                "Une erreur est survenue lors du chargement de la page.", 
-                "index.php?action=Home\Home", 
+                "Une erreur est survenue lors du chargement de la page.",
+                "index.php?action=Home\Home",
                 "Retour à la page d'accueil");
             return;
         }
 
         $comment = new Comment();
 
-        $comment->hydrate(array (
-            'id' => (int) $commentId,
+        $comment->hydrate(array(
+            'id'              => (int) $commentId,
             'publicationDate' => ($validation !== false && $validation !== null) ? date('Y-m-d H:i:s') : null,
-            'comment' => $commentString, 
-            'user' => $userRepository->getUser(Session::getActiveUserId()), 
-            'post' => $postRepository->getPost($postId)
+            'comment'         => $commentString,
+            'user'            => $userRepository->getUser(Session::getActiveUserId()),
+            'post'            => $postRepository->getPost($postId)
         ));
 
         $twig = TwigLoader::getEnvironment();
 
         //If the comment variable is empty
-        if($comment->getComment() === ""){
-            echo $twig->render('Admin\Comment\AdminComment.html.twig', [ 
-                'warningComment' => "Vous devez renseigner un commentaire", 
-                'commentId' => $comment->getId(), 
-                'postId' => $comment->getPost()->getId(), 
-                'commentString' => $comment->getComment(), 
+        if ($comment->getComment() === "") {
+            echo $twig->render('Admin\Comment\AdminComment.html.twig', [
+                'warningComment'  => "Vous devez renseigner un commentaire",
+                'commentId'       => $comment->getId(),
+                'postId'          => $comment->getPost()->getId(),
+                'commentString'   => $comment->getComment(),
                 'publicationDate' => $comment->getPublicationDate(),
-                'userFunction' => Session::getActiveUserFunction(),
-                'activeUser' => Session::getActiveUser()
+                'userFunction'    => Session::getActiveUserFunction(),
+                'activeUser'      => Session::getActiveUser()
             ]);
             return;
         }
@@ -80,7 +85,7 @@ class AdminCommentSave
         #region Function executions
 
         //If there is a commentId we update the comment field
-        if ($comment->getId() !== 0){
+        if ($comment->getId() !== 0) {
             $commentRepository->updateComment($comment);
             //We display the updated user list
             header("Location:index.php?action=Admin\Comment\AdminCommentList&pageNumber=1");
@@ -98,3 +103,4 @@ class AdminCommentSave
 
     #endregion
 }
+//end execute()
