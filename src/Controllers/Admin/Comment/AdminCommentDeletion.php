9ff9c4a30f49f7126353fc323ec907a942e7878f
gitcmartel <martel.c73@gmail.com>
2023-12-21 14:54:42 +0100

    Minor code style corrections and comments adding

--- src/Controllers/Admin/Comment/AdminCommentDeletion.php
+++ src/Controllers/Admin/Comment/AdminCommentDeletion.php
@@ -9,25 +9,28 @@ use Application\Lib\TwigWarning;
 class AdminCommentDeletion
 {
     #region Functions
-    public function execute()
+    /**
+     * Controller main function
+     */
+    public function execute() : void
     {
 
         #region Variables
-        
+
         $commentRepository = new CommentRepository();
         $comment = "";
 
         #endregion
 
         #region Conditions tests
-        
+
         //We display an warning message if one of the following conditions are false
 
         //If the active user is not an admin
-        if(! UserActiveCheckValidity::check(array('Administrateur'))){
+        if (UserActiveCheckValidity::check(array('Administrateur')) === false) {
             TwigWarning::display(
-                "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site", 
-                "index.php?action=Home\Home", 
+                "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site",
+                "index.php?action=Home\Home",
                 "Nous contacter");
             return;
         }
@@ -35,10 +38,10 @@ class AdminCommentDeletion
         $commentId = filter_input(INPUT_GET, 'commentId', FILTER_SANITIZE_NUMBER_INT);
 
         //If the commentId variable is not set
-        if ($commentId === false || $commentId === null){
+        if ($commentId === false || $commentId === null) {
             TwigWarning::display(
-                "Une erreur est survenue lors du chargement de la page.", 
-                "index.php?action=Home\Home", 
+                "Une erreur est survenue lors du chargement de la page.",
+                "index.php?action=Home\Home",
                 "Retour à la page d'accueil");
             return;
         }
@@ -46,10 +49,10 @@ class AdminCommentDeletion
         $comment = $commentRepository->getComment($commentId);
 
         //If the comment id is null
-        if($comment->getid() === null){
+        if ($comment->getid() === null) {
             TwigWarning::display(
-                "Une erreur est survenue lors du chargement de la page.", 
-                "index.php?action=Home\Home", 
+                "Une erreur est survenue lors du chargement de la page.",
+                "index.php?action=Home\Home",
                 "Retour à la page d'accueil");
             return;
         }
