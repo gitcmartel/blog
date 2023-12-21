13c10a691cf73cb2260f10a3cdbbad4350c9abca
gitcmartel <martel.c73@gmail.com>
2023-12-21 16:04:55 +0100

    Deleting incorrect portions of code

--- src/Controllers/Comment/CommentDeletion.php
+++ src/Controllers/Comment/CommentDeletion.php
@@ -9,26 +9,29 @@ use Application\Lib\TwigWarning;
 class CommentDeletion
 {
     #region Functions
-    public function execute()
+    /**
+     * Controller main function
+     */
+    public function execute(): void
     {
 
         #region Variables
-        
+
         $commentRepository = new CommentRepository();
         $comment = "";
 
         #endregion
 
         #region Conditions tests
-        
+
         //We display an warning message if one of the following conditions are false
         $commentId = filter_input(INPUT_GET, 'commentId', FILTER_SANITIZE_NUMBER_INT);
 
         //If the commentId variable is not set
-        if ($commentId === false || $commentId === null){
+        if ($commentId === false || $commentId === null) {
             TwigWarning::display(
-                "Une erreur est survenue lors du chargement de la page.", 
-                "index.php?action=Home\Home", 
+                "Une erreur est survenue lors du chargement de la page.",
+                "index.php?action=Home\Home",
                 "Retour à la page d'accueil");
             return;
         }
@@ -36,19 +39,19 @@ class CommentDeletion
         $comment = $commentRepository->getComment($commentId);
 
         //If the active user is not the comment's author
-        if(Session::getActiveUserId() !== $comment->getUser()->getId()){
+        if (Session::getActiveUserId() !== $comment->getUser()->getId()) {
             TwigWarning::display(
-                "Vous n'avez pas les droits requis pour effectuer cette action. Contactez l'administrateur du site", 
-                "index.php?action=Home\Home", 
+                "Vous n'avez pas les droits requis pour effectuer cette action. Contactez l'administrateur du site",
+                "index.php?action=Home\Home",
                 "Nous contacter");
             return;
         }
 
         //If the comment id is null
-        if($comment->getid() === null){
+        if ($comment->getid() === null) {
             TwigWarning::display(
-                "Une erreur est survenue lors du chargement de la page.", 
-                "index.php?action=Home\Home", 
+                "Une erreur est survenue lors du chargement de la page.",
+                "index.php?action=Home\Home",
                 "Retour à la page d'accueil");
             return;
         }
@@ -67,3 +70,4 @@ class CommentDeletion
     }
     #endregion
 }
+//end execute()
