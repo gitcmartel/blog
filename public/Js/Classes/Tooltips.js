5b1a5ef230cea8eb7f95c58604aa7fa21a43eb2c
gitcmartel <martel.c73@gmail.com>
2023-12-21 15:29:42 +0100

    Replacing data-bs-toggle by data-bs-toggle-tooltip attribute

--- public/Js/Classes/Tooltips.js
+++ public/Js/Classes/Tooltips.js
@@ -1,7 +1,7 @@
 class Tooltips {
   constructor () {
     var tooltipTriggerList = [].slice.call(
-      document.querySelectorAll('[data-bs-toggle="tooltip"]')
+      document.querySelectorAll('[data-bs-toggle-tooltip="tooltip"]')
     );
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
       return new bootstrap.Tooltip(tooltipTriggerEl)
