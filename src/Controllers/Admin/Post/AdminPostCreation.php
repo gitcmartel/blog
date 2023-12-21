d1e0a00c08a252325827afc2ca1f1b8797489b62
gitcmartel <martel.c73@gmail.com>
2023-12-21 15:01:30 +0100

    Minor code style corrections and comments adding

--- src/Controllers/Admin/Post/AdminPostCreation.php
+++ src/Controllers/Admin/Post/AdminPostCreation.php
@@ -11,7 +11,10 @@ use Application\Lib\TwigWarning;
 class AdminPostCreation
 {
     #region Functions
-    public function execute() 
+    /**
+     * Controller main function
+     */
+    public function execute() : void
     {
         #region Variables
         $post = "";
@@ -21,12 +24,12 @@ class AdminPostCreation
 
         #region Conditions tests
 
-        if(! UserActiveCheckValidity::check(array('Administrateur', 'Createur'))){
+        if (UserActiveCheckValidity::check(array('Administrateur', 'Createur')) === false) {
             TwigWarning::display(
-                "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site", 
-                "index.php?action=Home\Home", 
+                "Vous n'avez pas les droits requis pour accéder à cette page. Contactez l'administrateur du site",
+                "index.php?action=Home\Home",
                 "Nous contacter");
-            return;     
+            return;
         }
 
         $postId = filter_input(INPUT_GET, 'postId', FILTER_SANITIZE_NUMBER_INT);
@@ -34,19 +37,20 @@ class AdminPostCreation
         #endregion
 
         #region Function execution
-        if($postId === false || $postId === null){
-            $post = $postRepository->getPost($postId );
+        if ($postId === false || $postId === null) {
+            $post = $postRepository->getPost($postId);
         }
-        
+
         $twig = TwigLoader::getEnvironment();
-        
-        echo $twig->render('Admin\Post\AdminPost.html.twig', [ 
-            'post' => $post, 
-            'activeUser' => Session::getActiveUser(), 
+
+        echo $twig->render('Admin\Post\AdminPost.html.twig', [
+            'post' => $post,
+            'activeUser' => Session::getActiveUser(),
             'userFunction' => Session::getActiveUserFunction()
         ]);
-        
+
         #endregion
     }
     #endregion
 }
+//end execute()
